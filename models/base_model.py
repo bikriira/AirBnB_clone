c05c7b327bcf979e70a83f69dcba72125a8b0d83
bikriira <iradufashabikri@gmail.com>
2024-03-08 04:04:55 -0800

    [Feat] Add ability to re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,23 @@ import datetime
 class BaseModel():
     """ defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initialises all attributes"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if (kwargs):
+            # assign kwargd key and values to the object
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            self.created_at = datetime.datetime.strptime(
+                              kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.datetime.strptime(
+                              kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ returns the descriprion of the object
